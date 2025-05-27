f6c65e09c9242c00384084be9b724ecf69cec27a
redyeowoo <96750706+redyeowoo@users.noreply.github.com>
2025-05-27 19:04:43 +0900

    docs: EnemyHealthUI (#17)
    Co-authored-by: 박민규 <73173356+ps9319@users.noreply.github.com>

---
+++ Assets/01.Scripts/Enemy/EnemyUI/EnemyHealthUI_ES.cs
@@ -0,0 +1,86 @@
+using UnityEngine;
+
+public class EnemyHealth_Rect : MonoBehaviour
+{
+    [SerializeField] private float maxHealth = 100f;
+    private float currentHealth;
+
+    [Header("UI")]
+    [SerializeField] private RectTransform healthBarFill; // Fill 이미지의 RectTransform
+    [SerializeField] private GameObject healthBarUI;
+
+    private float originalWidth; // 시작 너비 기억
+    private float timeSinceLastHit = 0f;
+    private bool isRecovering = false;
+    private float recoverySpeed = 10f;
+    private float hideDelay = 1f;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+
+        if (healthBarFill != null)
+            originalWidth = healthBarFill.sizeDelta.x;
+
+        healthBarUI.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            TakeDamage(20f);
+        }
+
+        if (currentHealth < maxHealth)
+        {
+            timeSinceLastHit += Time.deltaTime;
+            if (timeSinceLastHit >= 5f)
+                isRecovering = true;
+        }
+
+        if (isRecovering)
+        {
+            RecoverHealth();
+        }
+    }
+
+    void TakeDamage(float damage)
+    {
+        currentHealth -= damage;
+        currentHealth = Mathf.Max(currentHealth, 0f);
+
+        UpdateHealthBar();
+
+        healthBarUI.SetActive(true);
+        timeSinceLastHit = 0f;
+        isRecovering = false;
+        CancelInvoke(nameof(HideHealthBar));
+    }
+
+    void RecoverHealth()
+    {
+        currentHealth += recoverySpeed * Time.deltaTime;
+        if (currentHealth >= maxHealth)
+        {
+            currentHealth = maxHealth;
+            isRecovering = false;
+            Invoke(nameof(HideHealthBar), hideDelay);
+        }
+
+        UpdateHealthBar();
+    }
+
+    void UpdateHealthBar()
+    {
+        float ratio = currentHealth / maxHealth;
+        Vector2 newSize = healthBarFill.sizeDelta;
+        newSize.x = originalWidth * ratio;
+        healthBarFill.sizeDelta = newSize;
+    }
+
+    void HideHealthBar()
+    {
+        healthBarUI.SetActive(false);
+    }
+}
