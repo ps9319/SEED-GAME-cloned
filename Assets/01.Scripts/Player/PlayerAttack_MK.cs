25e454bbcc97d031d1ea7adee6238dd74ed8dc11
박민규 <pmq0101@naver.com>
2025-06-03 02:44:12 +0900

    refactor: 전체적인 리팩토링
    attack collider -> weapon
    하드코딩 부분 교체
    Invoke문 coroutine으로 변경

--- Assets/01.Scripts/Player/PlayerAttack_MK.cs
+++ Assets/01.Scripts/Player/PlayerAttack_MK.cs
@@ -1,16 +1,21 @@
+using System.Collections;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class PlayerAttack_MK : MonoBehaviour
 {
     [SerializeField] private Animator animator;
-    [SerializeField] private GameObject attackCollider;
+    [SerializeField] private GameObject weapon;
 
+    private Weapon weaponComponent;
     public bool isAttacking = false;
-    private float attackCooldown = 1.0f; // 애니메이션 길이만큼
-    private float attackTimer = 0f;
-
+    private float attackCooldown; // 애니메이션 길이만큼
+    
     private PlayerMovement movement;
+    
+    private void Awake()
+    {
+        attackCooldown = weaponComponent.AttackSpeed;
+    }
 
     void Start()
     {
@@ -24,34 +29,28 @@ public class PlayerAttack_MK : MonoBehaviour
         // 공격 입력
         if (Input.GetMouseButtonDown(0) && !isAttacking)
         {
-            animator.SetTrigger("Attack");
             isAttacking = true;
-            attackTimer = attackCooldown;
-
-            EnableAttackHitbox(); // 공격 시작 시 활성화
-
-            Invoke(nameof(DisableAttackHitbox), 0.5f); // 공격 후 0.5초 뒤에 비활성화
-        }
-
-
-        // 공격 시간 경과 체크
-        if (isAttacking)
-        {
-            attackTimer -= Time.deltaTime;
-            if (attackTimer <= 0f)
-            {
-                isAttacking = false;
-            }
+            animator.SetTrigger("Attack");
+            StartCoroutine(Attack());
         }
     }
 
-    public void EnableAttackHitbox()
+    IEnumerator Attack()
     {
-        attackCollider.SetActive(true);
+        EnableAttackHitbox();
+        // 만약 애니메이션이 
+        yield return new WaitForSeconds(attackCooldown);
+        DisableAttackHitbox();
+    }
+    
+    private void EnableAttackHitbox()
+    {
+        weaponComponent.Hitbox.enabled = true;
     }
 
-    public void DisableAttackHitbox()
+    private void DisableAttackHitbox()
     {
-        attackCollider.SetActive(false);
+        weaponComponent.Hitbox.enabled = false;
+        isAttacking = false;
     }
 }
