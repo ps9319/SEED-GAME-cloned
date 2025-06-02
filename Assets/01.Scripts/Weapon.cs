a5472477f4de714c6f9b62f6c6eea84a6e0c95f3
박민규 <pmq0101@naver.com>
2025-06-03 02:20:50 +0900

    feat: 무기 스크립트 추가

---
+++ Assets/01.Scripts/Weapon.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    [SerializeField] private AttackInfos attackInfos;
+    [SerializeField] private Collider hitbox;
+    
+    public Collider Hitbox => hitbox;
+    public float Damage => attackInfos.damage;
+    // 무기마다 AttackSpeed를 애니메이션 재생 속도로
+    public float AttackSpeed => attackInfos.attackSpeed;
+
+    private void Awake()
+    {
+        attackInfos = GetComponent<AttackInfos>();
+    }
+    
+}
+
