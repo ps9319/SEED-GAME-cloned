a707cd03ec190be847ad9c4d06ae4b492205d2b8
박민규 <pmq0101@naver.com>
2025-06-03 03:04:08 +0900

    feat: EnemyHealth 추가

---
+++ Assets/01.Scripts/Enemy/Enemy/EnemyHealth.cs
@@ -0,0 +1,46 @@
+using System;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    [SerializeField] private EnemyInfos infos;
+    
+    private float maxHealth => infos.maxHealth;
+    private float currentHealth { get; set; }
+    private bool isDead { get; set; } = false;
+    
+    public event Action onDeath;
+    
+    private void Awake()
+    {
+        infos = GetComponent<EnemyInfos>();
+    }
+
+    private void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Weapon weapon = other.GetComponent<Weapon>();
+        if (weapon == null) return;
+        float attackDamage = weapon.Damage;
+        TakeDamage(attackDamage);
+    }
+
+    private void TakeDamage(float damage)
+    {
+        currentHealth -= damage;
+        if (currentHealth <= 0 && !isDead)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        onDeath();
+        isDead = true;
+    }
+}
